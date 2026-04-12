2282428eeb624636f403f50fdb9fd4c39fa8030f
李训迪 <lixundi@lixundideMacBook-Pro.local>
2026-04-12 23:36:40 +0800

    update

--- app_streamlit.py
+++ app_streamlit.py
@@ -23,7 +23,6 @@ st.set_page_config(page_title="Jedi News Scanner", page_icon="🕵️‍♂️",
 hide_st_style = """
             <style>
             footer {visibility: hidden;}
-            header {visibility: hidden;}
             </style>
             """
 st.markdown(hide_st_style, unsafe_allow_html=True)
